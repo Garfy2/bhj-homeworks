c29c8ae896b87b432fb2a849b5774c918b78a181
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2023-01-04 13:11:44 +0300

    client-state 1 to 3
    client-state 1 to 3

---
+++ client-state/popup/task.js
@@ -0,0 +1,28 @@
+const modalSub = document.getElementById('subscribe-modal');
+
+const closeModal = (e) => {
+    if (e.target.classList.contains('modal__close')) {
+        modalSub.classList.remove('modal_active');
+        document.cookie = 'modalclose=close';
+    }
+}
+modalSub.addEventListener('click', closeModal);
+
+const getCookie = (name) => {
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts
+            .pop()
+            .split(";")
+            .shift();
+    }
+}
+
+
+if (getCookie('modalclose')) {
+    modalSub.classList.remove('modal_active');
+    modalSub.removeEventListener('click', closeModal);
+} else {
+    modalSub.classList.add('modal_active');
+}
